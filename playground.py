01f1d9bc50d8e658ff39a28a55fa48db05d6814c
Ethan Chen <gradschool.hchen13@gmail.com>
2020-09-23 13:39:43 +0800

    amend

--- playground.py
+++ playground.py
@@ -1,6 +1,11 @@
 import imagenet
 
 if __name__ == '__main__':
+    if os.environ.get('AIBOX') is None:  # 本地开发
+        p = '/Users/ethan/datasets/ImageNet_tiny/'
+    else:                                # 训练机
+        p = '/media/ethan/DataStorage/ImageNet/'
+
     p = '/Users/ethan/datasets/ImageNet_tiny'
     dataset = imagenet.ImageNet(p)
     dataset.create_tfrecords(p, 192, 1000)
